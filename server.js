92142728c13640bb5eb19db857c3330337e1f042
Bailey Mashore <bailey@LAPTOP-AQND99TU.>
2025-05-23 18:08:52 -0700

    adding a wildcard subdomain to get cors to allow cross domain requests correctly

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/api/uploads', express.static('public/uploads'));
 
 const corsOptions = {
     // allows origin requests from local host 3000
-    origin: ["http://localhost:3000", `${process.env.FRONTEND_URL}`],
+    origin: ["http://localhost:3000", `${process.env.FRONTEND_URL}`, "https://my-math-frontend-baileym8203s-projects.vercel.app"],
     // allows cookies and authorization headers etc
     credentials: true,
 };
