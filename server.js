400ac3238155d48cd052e7f1a41c944a62895757
Bailey Mashore <bailey@LAPTOP-AQND99TU.>
2025-05-23 18:21:20 -0700

    adding the correct origin for a vercel preview to CORS then hiding via .env

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use('/api/uploads', express.static('public/uploads'));
 
 const corsOptions = {
     // allows origin requests from local host 3000
-    origin: ["http://localhost:3000", `${process.env.FRONTEND_URL}`, "https://my-math-frontend-baileym8203s-projects.vercel.app"],
+    origin: ["http://localhost:3000", `${process.env.FRONTEND_URL}`, `${process.env.FRONTEND_VERCEL_PREVIEW_URL}`],
     // allows cookies and authorization headers etc
     credentials: true,
 };
